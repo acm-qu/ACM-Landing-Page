2d274d1b6562946edc44562393ae1f9e7691c0ee
ansari <elansari1211@gmail.com>
2025-06-30 12:35:18 +0300

    feat: added navbar jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,22 @@
+// Navigation.jsx
 import '../styles/navigation.css'
 
-const Navigation = () => {
-  return (
-    <nav>
-      Navigation
+const Navigation = () => (
+  <div className="nav-container">
+    <nav className="navigation">
+      <div className="navigation-logo">acm</div>
+
+      <ul className="navigation-links">
+        <li><a href="#mission">Mission</a></li>
+        <li><a href="#events">Events</a></li>
+        <li><a href="#internship">Internship</a></li>
+        <li><a href="#team">Team</a></li>
+      </ul>
+
+      <a href="#join" className="join">Join Us!</a>
     </nav>
-  )
-}
+  </div>
+)
 
 export default Navigation
+
